3c8b6faf763a89abbf19bceb53b7c56e3589cca6
haruki <soccer_haruki15@me.com>
2023-11-14 14:45:34 -0800

    Updated Skill section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,14 +4,10 @@ import "slick-carousel/slick/slick-theme.css";
 import SkillCard from "./SkillCard.js";
 
 import javascript from "../assets/skills/javascript.svg";
-import bash from "../assets/skills/bash.svg";
-import linux from "../assets/skills/linux.svg";
 import python from "../assets/skills/python.svg";
 import reactIcon from "../assets/skills/react.svg";
-import windows from "../assets/skills/windows.svg";
 import docker from "../assets/skills/docker.svg";
 import git from "../assets/skills/git.svg";
-import sass from "../assets/skills/sass.svg";
 import ts from "../assets/skills/typescript.svg";
 import kubernetes from "../assets/skills/kubernetes.svg";
 import aws from "../assets/skills/aws.svg";
@@ -19,11 +15,10 @@ import java from "../assets/skills/java.svg";
 import redux from "../assets/skills/redux.svg";
 import gc from "../assets/skills/gc.svg";
 import jira from "../assets/skills/jira.svg";
-import swift from "../assets/skills/swift.svg";
-import kotlin from "../assets/skills/kotlin.svg";
 import spring from "../assets/skills/spring.svg";
 import azure from "../assets/skills/azure.svg";
 import tailwind from "../assets/skills/tailwind.svg";
+import figma from "../assets/skills/figma.svg";
 
 export default function Skills(props) {
   const isDarkMode = props.isDarkMode;
@@ -47,32 +42,26 @@ export default function Skills(props) {
       </p>
       <div className="mt-4 px-4">
         <Slider {...settings}>
-          <SkillCard name="Javascript" experience="2+ years" img={javascript} />
-          <SkillCard name="TypeScript" experience="2+ years" img={ts} />
+          <SkillCard name="Javascript" experience="3+ years" img={javascript} />
+          <SkillCard name="TypeScript" experience="3+ years" img={ts} />
           <SkillCard name="Python" experience="Familiar" img={python} />
-          <SkillCard name="Java" experience="2+ years" img={java} />
-          <SkillCard name="Swift" experience="<1 year" img={swift} />
-          <SkillCard name="Kotlin" experience="<1 year" img={kotlin} />
-          <SkillCard name="Bash" experience="2+ years" img={bash} />
+          <SkillCard name="Java" experience="3+ years" img={java} />
 
-          <SkillCard name="Sass" experience="2+ years" img={sass} />
-          <SkillCard name="Tailwind" experience="2+ years" img={tailwind} />
-          <SkillCard name="React" experience="2+ years" img={reactIcon} />
-          <SkillCard name="Redux" experience="2+ years" img={redux} />
-          <SkillCard name="Spring" experience="2+ years" img={spring} />
+          <SkillCard name="Tailwind" experience="3+ years" img={tailwind} />
+          <SkillCard name="React" experience="3+ years" img={reactIcon} />
+          <SkillCard name="Redux" experience="3+ years" img={redux} />
+          <SkillCard name="Spring" experience="3+ years" img={spring} />
 
           <SkillCard name="Docker" experience="2+ years" img={docker} />
           <SkillCard name="Kubernetes" experience="Familiar" img={kubernetes} />
 
-          <SkillCard name="AWS" experience="2+ years" img={aws} />
+          <SkillCard name="AWS" experience="3+ years" img={aws} />
           <SkillCard name="Azure" experience="Familiar" img={azure} />
           <SkillCard name="Google Cloud " experience="Familiar" img={gc} />
 
-          <SkillCard name="Windows" experience="2+ years" img={windows} />
-          <SkillCard name="Linux" experience="2+ years" img={linux} />
-
-          <SkillCard name="Git" experience="2+ years" img={git} />
-          <SkillCard name="Jira" experience="2+ years" img={jira} />
+          <SkillCard name="Git" experience="3+ years" img={git} />
+          <SkillCard name="Jira" experience="3+ years" img={jira} />
+          <SkillCard name="Figma" experience="3+ years" img={figma} />
         </Slider>
       </div>
     </div>
